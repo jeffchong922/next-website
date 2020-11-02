ff9c2306873b3f0a9473b7db0096b295243df8ec
jeffchong <jeffchong922@outlook.com>
2020-11-02 16:25:18 +0800

    文档展示

--- lib/articles.ts
+++ lib/articles.ts
@@ -1,6 +1,8 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
+import remark from 'remark'
+import html from 'remark-html'
 import { fisherYates } from '../utils/tools'
 
 const articlesDirectory = path.join(process.cwd(), 'data/articles')
@@ -33,4 +35,36 @@ export function getRandomArticleList () {
   })
 
   return fisherYates(articleList)
+}
+
+export function getAllArticleIds () {
+  const fileNames = fs.readdirSync(articlesDirectory)
+
+  return fileNames.map(fileName => {
+    return {
+      params: {
+        id: fileName.replace(/.md$/, '')
+      }
+    }
+  })
+}
+
+export async function getArticleDataById (id: string) {
+  const fullPath = path.join(articlesDirectory, `${id}.md`)
+  const fileContents = fs.readFileSync(fullPath, 'utf-8')
+
+  const matterResult = matter(fileContents)
+
+  const processContent = await remark()
+    .use(html)
+    .process(matterResult.content)
+
+  const contentHtml = processContent.toString()
+
+  return {
+    id,
+    contentHtml,
+    title: id,
+    ...matterResult.data
+  }
 }
