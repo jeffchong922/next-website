bee28ea599648e2e7ee31a1a6c25343367528e77
jeffchong <jeffchong922@outlook.com>
2020-11-06 22:06:20 +0800

    引入nprogress

---
+++ helpers/nprogress.ts
@@ -0,0 +1,40 @@
+// https://github.com/rstacruz/nprogress
+import Router from 'next/router'
+import nProgress, { NProgressOptions } from 'nprogress'
+
+// 自定义默认配置
+nProgress.configure({
+  showSpinner: false
+})
+
+/**
+ * 全局 _app 执行即可
+ * 
+ * 确保引入相应 css 文件
+ */
+export default function useNProgress () {
+  // Fires when a route starts to change
+  Router.events.on('routeChangeStart', (url) => {
+    console.log(`Loading: ${url}`)
+    nProgress.start()
+  })
+
+  // Fires when a route changed completely
+  Router.events.on('routeChangeComplete', (url) => {
+    nProgress.done()
+  })
+
+  // Fires when there's an error when changing routes, or a route load is cancelled
+  Router.events.on('routeChangeError', (err, url) => {
+    nProgress.done()
+  })  
+}
+
+/**
+ * 配置 nprogress
+ */
+export function setNProgress (config: Partial<NProgressOptions>) {
+  nProgress.configure(config)
+}
+
+export const nPInstance = nProgress
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,22 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 import { ThemeProvider } from 'theme-ui'
 import theme from '../config/theme'
+import useNProgress from '../helpers/nprogress'
+
+useNProgress()
 
 function MyApp ({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps}/>
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>Jeff - 前端萌新🧐</title>
+        <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps}/>
+      </ThemeProvider>
+    </>
   )
 }
 
