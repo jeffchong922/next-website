f3c191af55096db65c4052ff35d2b6a6ad948c53
jeffchong <jeffchong922@outlook.com>
2020-11-08 17:51:59 +0800

    添加相关标签

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -1,14 +1,18 @@
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next'
 import hydrate from 'next-mdx-remote/hydrate'
-import { Box } from 'theme-ui'
+import { Box, Flex, Text } from 'theme-ui'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import makeComponents from '../../config/mdxComponents'
 import Layout from '../../layout'
-import { getAllArticleIds, getArticleById, MdxArticle } from '../../libs/articles'
+import { getAllArticleIds, getArticleById } from '../../libs/articles'
 import makeDocTitle from '../../helpers/doc-title'
 import Custom404 from '../404'
 import LoadingPage from '../../components/shared/LoadingPage'
+import ResourceTitle from '../../components/shared/ResourceTitle'
+import BBBox from '../../components/shared/BBBox'
+import NormalLink from '../../components/shared/NormalLink'
+import { transformStrForLink, transformStrForShow } from '../../helpers/name-link'
 
 export type Query = {
   slug: string
@@ -27,6 +31,10 @@ export type FetchError = {
 export type ArticleProps = {
   errMsg: string
   mdxSource: any
+  frontMatter: {
+    title: string
+    tags: string[]
+  }
   localComponents: string[]
 }
 
@@ -51,15 +59,20 @@ export const getStaticProps: GetStaticProps<ArticleProps, Query> = async ({
   let result: ArticleProps = {
     errMsg: '',
     mdxSource: null,
+    frontMatter: null,
     localComponents: []
   }
   const article = await getArticleById(params.slug)
   if (article.errMsg) {
     result.errMsg = article.errMsg
   } else {
-    const { mdxSource, localComponents } = article.data
+    const { mdxSource, localComponents, title, tags } = article.data
     result.mdxSource = mdxSource
     result.localComponents = localComponents
+    result.frontMatter = {
+      title,
+      tags
+    }
   }
   
   return {
@@ -71,6 +84,7 @@ export const getStaticProps: GetStaticProps<ArticleProps, Query> = async ({
 const Article = ({
   errMsg,
   mdxSource,
+  frontMatter,
   localComponents
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
   const router = useRouter()
@@ -88,12 +102,39 @@ const Article = ({
   return (
     <Layout showArticleProgress={true}>
       <Head>
-        <title>{makeDocTitle('标题')}</title>
+        <title>{makeDocTitle(frontMatter.title)}</title>
       </Head>
 
-      <Box sx={{ px: ['4', '16', '24'], maxWidth: '5xl' }}>
-        {content}
-      </Box>
+      <BBBox>
+        <Box sx={{ px: ['4', '16', '24'], maxWidth: '5xl' }}>
+          <h1 style={{ marginBottom: '3.6rem', fontSize: '2.8rem' }}>{frontMatter.title}</h1>
+          {content}
+        </Box>
+      </BBBox>
+
+      <ResourceTitle>🔗 相关标签</ResourceTitle>
+      <BBBox>
+        <Flex sx={{ flexWrap: 'wrap', pt: '8' }}>
+          {
+            frontMatter.tags.map(tag => (
+              <Text key={tag} sx={{
+                width: ['1/3', '1/4'],
+                mb: '8',
+                textAlign: 'center', fontSize: ['lg', 'xl'], fontWeight: 'bold',
+                color: 'text',
+                textDecoration: 'underline',
+                transition: 'color .3s',
+                ":hover": {
+                  color: 'primary'
+                }
+              }}>
+                <NormalLink href={`/tags/${transformStrForLink(tag)}`}>#{transformStrForShow(tag)}</NormalLink>
+              </Text>
+            ))
+          }
+        </Flex>
+      </BBBox>
+
     </Layout>
   )
 }
